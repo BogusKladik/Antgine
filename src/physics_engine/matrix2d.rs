d490dfeef8fed3ced2ce2b2ecccd57a382d73b22
BogusKladik <boguskladik@gmail.com>
2022-04-11 08:45:39 +0000

    returned matrix

---
+++ src/physics_engine/matrix2d.rs
@@ -0,0 +1,27 @@
+use std::default;
+
+use super::vec2d::Vec2D;
+
+struct Matrix2D {
+    data: [[f32; 2]; 2],
+}
+
+impl Default for Matrix2D {
+    fn default() -> Self {
+        Matrix2D::new(0.0, 0.0, 0.0, 0.0)
+    }
+}
+
+impl Matrix2D {
+    pub fn new(a11: f32, a12: f32, a21: f32, a22: f32) -> Matrix2D {
+        let data = [[a11, a12], [a21, a22]];
+        Matrix2D { data }
+    }
+
+    pub fn multiply_vec2d(&self, vector: &Vec2D) -> Vec2D {
+        Vec2D::new(
+            self.data[0][0] * vector.x + self.data[0][1] * vector.y,
+            self.data[1][0] * vector.x + self.data[1][1] * vector.y,
+        )
+    }
+}
--- src/physics_engine/mod.rs
+++ src/physics_engine/mod.rs
@@ -6,6 +6,6 @@ pub mod traits;
 
 pub mod vec2d;
 
-// pub mod matrix2d;
+pub mod matrix2d;
 
 pub mod angle;
